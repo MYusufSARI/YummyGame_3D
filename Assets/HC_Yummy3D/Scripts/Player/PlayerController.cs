3b51a440252543b701e63e53f402b626bdb0e7ef
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-07-29 20:08:52 +0300

    Added playerController logic

---
+++ Assets/HC_Yummy3D/Scripts/Player/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header(" Settings ")]
+    [SerializeField] private float screenPositionFollowThershold;
+    [SerializeField] private float moveSpeed;
+    private Vector3 clickedScreenPosition;
+
+
+
+    private void Update()
+    {
+        ManageControl();
+    }
+
+
+    private void ManageControl()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickedScreenPosition = Input.mousePosition;
+        }
+
+        else if (Input.GetMouseButton(0))
+        {
+            // Calculate the difference in Screen Position
+            Vector3 difference = Input.mousePosition - clickedScreenPosition;
+
+            Vector3 direction = difference.normalized;
+
+            float maxScreenDistance = screenPositionFollowThershold * Screen.height;
+
+            if (difference.magnitude > maxScreenDistance)
+            {
+                clickedScreenPosition = Input.mousePosition - direction * maxScreenDistance;
+                difference = Input.mousePosition - clickedScreenPosition;
+            }
+
+            difference /= Screen.width;
+
+            difference.z = difference.y;
+            difference.y = 0;
+
+            Vector3 targetPosition = transform.position + difference * moveSpeed * Time.deltaTime;
+
+            transform.position = targetPosition;
+        }
+    }
+}
