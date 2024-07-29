a96dc92ef4e366a8838e026ba897022740ab7f25
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-07-29 21:06:36 +0300

    Added the logic of growing

---
+++ Assets/HC_Yummy3D/Scripts/Collectible.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    [Header(" Settings ")]
+    [SerializeField] private float _size;
+
+
+
+    public float GetSize()
+    {
+        return _size;
+    }
+}
